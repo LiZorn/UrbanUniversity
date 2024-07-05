c04198ab848355aa4b8ed43ce10b8512c11ab93b
LiZorn <kazakova_law@mail.ru>
2024-07-05 21:30:47 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
-course="Python, "
-task="12, "
-hour="1.5, "
-time="0.125"
-print("Курс: ",course,"всего задач:", task, "затрачено часов: ", hour, "среднее время выполнения ", time, " часа.")
+course="Python"
+task=12
+hour=1.5
+time=hour/task
+print("Курс:", course, ',',"всего задач:", task,',', "затрачено часов:", hour,',', "среднее время выполнения", time, "часа",'.')
