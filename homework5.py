d88ea31c72bae1c0958292c6fb10cd4932e7f247
LiZorn <kazakova_law@mail.ru>
2024-07-05 21:09:17 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var=1, True, "Moon"
+print(immutable_var)
+immutable_var=([1, True], "Moon")*2
+print(immutable_var)
+mutable_list=["apple", 1, False]
+print(mutable_list)
+mutable_list[0]='peach'
+print(mutable_list)
