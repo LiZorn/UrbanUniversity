26a6cfa903bf964d7d085544b5783d872068f3bf
LiZorn <kazakova_law@mail.ru>
2024-07-06 16:25:48 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name="Liza"
 print("Name: ",name)
-age="24"
+age=24
 print("Age: ", age)
-age=age+"5"
+age=age+5
 print("New Age: ", age)
-is_student="True"
+is_student=True
 print("Is Student: ", is_student)
