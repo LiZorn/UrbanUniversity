be116b3d5064c0f8c7987f7d90b83d77bff47608
LiZorn <kazakova_law@mail.ru>
2024-08-05 22:18:29 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,13 +1,11 @@
-import re
-
 def single_root_words(root_word, *other_words):
     same_words = []
-    for i in other_words:
-        if i in other_words:
-            re.search(root_word, i)
-            same_words.append(i)
+    words = list(other_words)
 
-    return same_words
+    for i in range(len(words)):
+        if root_word.lower() in words[i].lower() or words[i].lower() in root_word.lower():
+            same_words.append(words[i])
+    return (same_words)
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
